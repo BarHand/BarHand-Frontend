ff4827be8ca16356fa5248a4e3b48d5324e584b8
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2022-10-01 01:00:39 -0500

    feat: update

--- src/security/page/sign-in.vue
+++ src/security/page/sign-in.vue
@@ -1,37 +1,76 @@
+
+
 <template>
-  <div class="container">
-    <h1>Login to BarHand</h1>
-    <div class="col-md-9">
-      <form>
-        <div class="form-group">
-          <input
-              type="email"
-              class="form-control"
-              id="email"
-              placeholder="Enter email"
-          />
-        </div>
-        <div class="form-group">
-          <input
-              type="password"
-              class="form-control"
-              id="password"
-              placeholder="Password"
-          />
-        </div>
-        <br>
-        <div>
-          <button type="submit" class="btn btn-primary">Sign in</button>
-        </div>
-      </form>
+  <div class="card">
+    <h5>Login to BarHand</h5>
+    <div class="field">
+      <label for="Email">Email</label>
+      <input id="Email" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-4">
     </div>
+    <div class="field">
+      <label for="password1">Password</label>
+      <input id="password1" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-4">
+    </div>
+    <pv-button label="Submit" />
   </div>
+
 </template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
 
-<style>
+<style scoped>
+.p-button {
+  margin-right: .5rem;
+}
+
+.p-buttonset {
+.p-button {
+  margin-right: 0;
+}
+}
+
+.sizes {
+.button {
+  margin-bottom: .5rem;
+  display: block;
+
+&:last-child {
+   margin-bottom: 0;
+ }
+}
+}
+
+@media screen and (max-width: 640px) {
+  .p-button {
+    margin-bottom: .5rem;
+
+&:not(.p-button-icon-only) {
+   display: flex;
+   width: 100%;
+ }
+}
+
+.p-buttonset {
+.p-button {
+  margin-bottom: 0;
+}
+}
+}
+.card{
+  justify-content: center;
+  size: auto;
+  text-align: center;
+}
+.field-radiobutton{
+  display:flex;
+  justify-content: center;
+  text-align: center;
+}
+
 
 </style>
+
--- src/security/page/sign-up.vue
+++ src/security/page/sign-up.vue
@@ -1,70 +1,93 @@
 
 
 <template>
-  <div class="container">
-    <h1>Register BarHand</h1>
-    <div class="col-md-9">
-      <form>
-        <div class="form-group">
-          <input
-              type="name"
-              class="form-control"
-              id="fullname"
-              placeholder="Full Name"
-          />
-        </div>
-        <div class="form-group">
-          <input
-              type="email"
-              class="form-control"
-              id="email"
-              placeholder="Enter email"
-          />
-        </div>
-        <div class="form-group">
-          <input
-              type="password"
-              class="form-control"
-              id="password"
-              placeholder="Password"
-          />
-        </div>
-        <div class="form-group">
-          <input
-              type="password"
-              class="form-control"
-              id="reenterpasword"
-              placeholder="Reenterpassword"
-          />
-        </div>
-        <div>
-          <h5 id="single">Client type</h5>
-          <SelectButton v-model="Client" :options="Bussiness" aria-labelledby="single" />
-        </div>
-        <br>
-        <p>
-          <label>
-            <input type="radio" name="optionRadios" id="ClientButton" value="option1"> "Client"
-          </label>
-          <br>
-          <label>
-            <input type="radio" name="optionRadios" id="BussinessButton" value="option1"> "Bussiness"
-          </label>
-        </p>
-        <div>
-          <button type="submit" class="btn btn-primary">Sign in</button>
-        </div>
-      </form>
+    <div class="card">
+      <h5>Register to BarHand</h5>
+      <div class="field">
+        <label for="FullName">Full name</label>
+        <input id="FullName" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-4">
+      </div>
+      <div class="field">
+        <label for="Email">Email</label>
+        <input id="Email" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-4">
+      </div>
+      <div class="field">
+        <label for="password1">Password</label>
+        <input id="password1" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-4">
+      </div>
+      <div class="field">
+        <label for="password2">Reenterpassword</label>
+        <input id="password2" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-4">
+      </div>
+      <h5>Client Type</h5>
+      <div class="field-radiobutton">
+        <input type="radio" name="type" id="type1">
+        <label for="type1">Client</label>
+      </div>
+      <div class="field-radiobutton">
+        <input type="radio" name="type" id="type2">
+        <label for="type2">Bussiness</label>
+      </div>
+      <pv-button label="Submit" />
     </div>
-  </div>
+
 </template>
 
 <script>
 export default {
+
 };
 </script>
 
-<style>
+<style scoped>
+.p-button {
+  margin-right: .5rem;
+}
+
+.p-buttonset {
+  .p-button {
+    margin-right: 0;
+  }
+}
+
+.sizes {
+  .button {
+    margin-bottom: .5rem;
+    display: block;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
+
+@media screen and (max-width: 640px) {
+  .p-button {
+    margin-bottom: .5rem;
+
+    &:not(.p-button-icon-only) {
+      display: flex;
+      width: 100%;
+    }
+  }
+
+  .p-buttonset {
+    .p-button {
+      margin-bottom: 0;
+    }
+  }
+}
+.card{
+  justify-content: center;
+  size: auto;
+  text-align: center;
+}
+.field-radiobutton{
+  display:flex;
+  justify-content: center;
+  text-align: center;
+}
+
 
 </style>
 
