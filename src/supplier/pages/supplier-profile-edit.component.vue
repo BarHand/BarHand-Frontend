d5f3e1787b0311c11b310d170a29d84de85d5f53
YENI <u20201b032@upc.edu.pe>
2022-10-18 17:54:35 -0500

    feat: update supplier-profile-edit component

--- src/supplier/pages/supplier-profile-edit.component.vue
+++ src/supplier/pages/supplier-profile-edit.component.vue
@@ -1,145 +1,170 @@
 <template>
-  <RouterView></RouterView>
+<body>
   <div class="home">
-    <div class="background" style="height: 220vh; margin: 0;padding: 0">
-      <div class="header">
-        <div class="buttons">
-          <div class="button">
-            <RouterLink to="/contacts">
-              <pv-button label="Contacts" style="background-color: #9acb3c" />
-            </RouterLink>
-          </div>
-          <div class="button">
-            <RouterLink to="/home-store">
-              <pv-button label="Products" style="background-color: #9acb3c" />
-            </RouterLink>
-          </div>
-          <div class="button">
-            <RouterLink to="/stores">
-              <pv-button label="Stores" style="background-color: #9acb3c" />
-
-            </RouterLink>
-          </div>
-        </div>
-      </div>
+    <div>
+      <div class="container">
+        <img :src="this.supplier.image" >
+        <form action="#">
       <div id="sup">
         <div>
-          {{supplier}}
-          <span>
-            <h5>Supplier Name</h5>
-            <pv-input-text type="text" v-model="supplier.supplierName" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Name</h5>
-            <pv-input-text type="text" v-model="supplier.name" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Last Name</h5>
-            <pv-input-text type="text" v-model="supplier.lastName" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Image</h5>
-            <pv-input-text type="text" v-model="supplier.image" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Email</h5>
-            <pv-input-text type="text" v-model="supplier.email" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Address</h5>
-            <pv-input-text type="text" v-model="supplier.address" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-          </span>
+          <div class="user-detail">
+            <div class="input-box">
+              <span class="details"> Full Name </span>
+              <pv-input-text  v-model="supplier.name" style="width: 100%" type="text" placeholder="Enter your name" required></pv-input-text>
+            </div>
+
+            <div class="input-box">
+              <span class="details"> Last Name </span>
+              <pv-input-text v-model="supplier.lastName" style="width: 100%" type="text" placeholder="Enter your last name" required></pv-input-text>
+            </div>
+
+            <div class="input-box">
+              <span class="details"> Business Name </span>
+              <pv-input-text v-model="supplier.supplierName" style="width: 100%" type="text" placeholder="Enter your business name"
+                             required></pv-input-text>
+            </div>
+
+            <div class="input-box">
+              <span class="details"> Email </span>
+              <pv-input-text v-model="supplier.email" style="width: 100%" type="text" placeholder="Enter your email" required></pv-input-text>
+            </div>
+
+            <div class="input-box">
+              <span class="details"> Address </span>
+              <pv-input-text v-model="supplier.address" style="width: 100%" type="text" placeholder="Enter your address" required></pv-input-text>
+            </div>
+
+            <div class="input-box">
+              <span class="details"> Phone </span>
+              <pv-input-text v-model="supplier.phone" style="width: 100%" type="text" placeholder="Enter your number phone" required></pv-input-text>
+            </div>
+
+            <div class="input-box">
+              <span class="details">  Image </span>
+              <pv-input-text v-model="supplier.image" style="width: 100%" type="text" placeholder="Enter your url business image"
+                             required></pv-input-text>
+            </div>
+
+
+            <div  class="input-box">
+              <span class="details"> RUC </span>
+              <pv-input-text v-model="supplier.ruc" style="width: 100%" type="text" placeholder="Enter your RUC" required> </pv-input-text>
+            </div>
+
+            <div  class="input-box">
+              <span class="details"> Category </span>
+              <pv-input-text v-model="supplier.category" style="width: 100%" type="text" placeholder="Enter your category business" required> </pv-input-text>
+            </div>
+
+            <div class="input-box">
+              <span class="details"> Description </span>
+              <pv-text-area v-model="supplier.description" style="width: 100%" type="text" placeholder="Enter your description business" required> </pv-text-area>
+            </div>
+          </div>
         </div>
-        <div>
-          <span>
-            <h5>RUC</h5>
-            <pv-input-text type="text" v-model="supplier.ruc" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Category</h5>
-            <pv-input-text type="text" v-model="supplier.category" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Description</h5>
-            <pv-input-text type="text" v-model="supplier.description" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Phone</h5>
-            <pv-input-text type="text" v-model="supplier.phone" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Password</h5>
-            <pv-input-text type="text" v-model="supplier.password" style=" border-color: black; border-radius: 1rem;background-color: white"/>
-            <h5>Guardar datos</h5>
-                  <router-link to="">
-            <pv-button id="button" label="Guardar" style="background-color: #9acb3c" @click="update"/>
-          </router-link>
-          </span>
-        </div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZeBUJui5XoVkOvuGDRqFXL1H11-CTghVcyQ&usqp=CAU" >
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSFLQO_8mDXjrBkx3Xwq4vogDBfCPuRbhGMw&usqp=CAU" >
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPpDnybeHWza89Acxse8KtQSKxImJQgtVoig&usqp=CAU" >
       </div>
+        </form>
+        <router-link :to="{name: 'profile-supplier'}">
+          <pv-button id="button" label="Save" style="background-color: #9acb3c; width:100%" @click="update"/>
+        </router-link>
+    </div>
     </div>
   </div>
+</body>
 </template>
 
 <script>
 import {SuppliersApiService} from "../services/suppliers-api.service";
-import {ProductsApiService} from "../services/products-api.service";
+
+import {useRoute} from "vue-router";
 
 export default {
   name: "profile.component",
   data() {
     return {
+      id: "",
       supplier:{},
       supplierService: null,
     }
   },
   created()
   {
-    this.supplierService = new SuppliersApiService();
+    const route= useRoute();
+    this.id=route.params.id;
 
+    console.log(this.id)
+    this.supplierService = new SuppliersApiService();
+    this.supplierService.getById(this.id).then((response) => {    //getById configurar para inicio de sesion copio el id del que inicio sesión
+      this.supplier = response.data;
+    });
   },
   methods: {
     update() {
-      this.supplierService.update(1,this.supplier)   //getById configurar para inicio de sesion copio el id del que inicio sesión
+      this.supplierService.update(this.id,this.supplier)   //getById configurar para inicio de sesion copio el id del que inicio sesión
     }
   }
 }
 </script>
 
 <style scoped>
-.header {
-  display: grid;
+* {
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Roboto";
+}
 
-}
-.background{
-  background-color: #afbaca;
-}
-  #sup {
-    display: flex;
-    height: 50vh;
+body {
 
-    align-items: center;
-  }
-#sup img {
-  width:220px;
-  position: center;
-  top:0px;
-  left:200px;
-}
-#sup div {
-  display: grid;
-  position:relative;
-  top:-50px;
-  left:50px;
-  width: 35%;
-  height: 100%;
-  right: 5%;
-  opacity: 90%;
-  align-items: inherit;
-  text-align: center;
-}
-#button {
-  width: 50%;
-}
-.buttons{
-  width: 70vw;
-  display:inline-flex;
-  justify-content: end;
-
-}
-.button{
-  align-items: center;
-  height:80px;
-  width: 120px;
-  margin: 1.2rem 4rem 3rem 4rem;
   display: flex;
-  text-align: center;
-  font-size: x-large;
+  justify-content: center;
+  align-items: center;
+  background-color: #afbaca;
+
+  background-size: 100%;
+
+  opacity: 0.9;
 }
 
+.container {
+  max-width: 700px;
+  width: 100%;
+  background: #fff;
+  padding: 25px 30px;
+  border-radius: 5px;
+}
+
+.container .title {
+  font-size: 25px;
+  font-weight: 500;
+  position: relative;
+}
+
+.container .title::before {
+  content: ' ';
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 3px;
+  width: 30px;
+  background: -moz-linear-gradient(135deg, black, white);
+}
+
+.container form .user-detail {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+form .user-detail .input-box {
+  width: calc(100% / 2 - 20px);
+}
+
+form .TypeUser {
+  display: flex;
+  position: relative;
+  margin: 14px 0;
+}
+img {
+  width: 100%;
+}
 </style>
