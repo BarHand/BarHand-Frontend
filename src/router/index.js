357f33dbd0f425eb85e3a82810b5f37bdf157b6a
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2022-10-20 06:35:45 -0500

    feat: update

--- src/router/index.js
+++ src/router/index.js
@@ -15,42 +15,11 @@ const router = createRouter({
       component: () => import('../views/navbar.vue')
     },
     {
-      path: "/sign-up",
-      name: "sign-up",
-      component: ()=>import('../security/page/sign-up.vue'),
-    },
-    {
-      path: "/sign-in",
-      name: "sign-in",
-      component: ()=>import('../security/page/sign-in.vue'),
-    },
-    {
-      path: "/home-store",
-      name: "home-store",
-      component: ()=>import('../store/pages/home-store.component.vue'),
-    },
-    {
-      path: "/profile-supplier/:id",
-      name: "profile-supplier",
-      component: ()=>import('../supplier/pages/profile-supplier.component.vue'),
-    },
-    {
-      path: "/supplier-profile-edit/:id",
-      name: "supplier-profile-edit",
-      component: ()=>import('../supplier/pages/supplier-profile-edit.component.vue'),
-    },
-    {
-      path: "/edit/:id",
-      name: "edit",
-      component: ()=>import('../store/pages/store-supplier-edit.component.vue'),
-    },
-    {
-      path: '/add',
-      name: 'add',
-      component: ()=>import('../store/pages/store-supplier-add.component.vue'),
-    },
-
-  ]
-})
+      path:'/chat',
+      name: 'chat',
+      component:()=> import('../chats/chat.vue')
+    }
+    ]}
+)
 
 export default router
