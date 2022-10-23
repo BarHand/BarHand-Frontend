9ea1071b31840670fa90eeb791be54f1b44fba50
Rojoxt <delamarzp@gmail.com>
2022-10-23 11:23:42 -0500

    feat: refactor store profile

--- src/store/pages/store-profile.component.vue
+++ src/store/pages/store-profile.component.vue
@@ -17,9 +17,9 @@
         <b>Phone:</b> {{store.phone}}<br>
       </template>
       <template #footer>
-
-        <pv-button v-on:click="editStore()" icon="pi pi-user-edit" label="Edit"  style="width: 100%"/>
-
+        <router-link :to="{ name: 'store-profile-edit'}"><!--, id}-->
+        <pv-button icon="pi pi-user-edit" label="Edit"  style="width: 100%"/>
+        </router-link>
       </template>
     </pv-card>
   </div>
@@ -27,36 +27,28 @@
 
 <script>
 import {StoresApiService} from "@/store/services/stores-api.service";
-import {useRoute} from "vue-router";
 
 export default {
   name: "store-profile",
   data() {
     return {
+      id:1,
       store: {},// esta es la manera correcta // nunca poner null
       storeService: null,
-      id: Number,
 
     }
   },
   created() {
-    const route= useRoute();
-    this.id = route.params.id;
-    this.getStore(this.id);
+    //const route= useRoute();
+    this.id = 1;//route.params.id;
+
+    //console.log(this.id)
+    this.storeService = new StoresApiService();
+    this.storeService.getById(this.id).then((response) => {    //getById configurar para inicio de sesion copio el id del que inicio sesión
+      this.store = response.data;
+    });
+
   },
-
-  methods: {
-    getStore(storeId){
-      this.storeService = new StoresApiService();
-      this.storeService.getById(storeId).then((response) => {    //getById configurar para inicio de sesion copio el id del que inicio sesión
-        this.store = response.data;
-      });
-
-    },
-    editStore(){
-      this.$router.push({name: 'store-profile-edit'});
-    }
-}
 }
 </script>
 
