9f53751d4c741a2c50dfdbd65889c3ef288dd989
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2022-11-12 09:05:39 -0500

    feat: update

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -18,9 +18,9 @@ export default {
       user: String,
       navigationSupplier: [
         {label: "Home", icon: 'pi pi-fw pi-home', to: {name:'supplier-home'}},
-        {label: "Plans", icon: 'pi pi-fw pi-book', to: {name:'supplier-home'}},
+        {label: "Plans", icon: 'pi pi-fw pi-book', to: {name:'supplier-plans'}},
         {label: "Notification", icon: 'pi pi-fw pi-bell', to: {name:'supplier-home'}},
-        {label: "Inventory", icon: 'pi pi-fw pi-box', to: {name:'supplier-home'}},
+        {label: "Inventory", icon: 'pi pi-fw pi-box', to: {name:'supplier-inventory'}},
         {label: "Profile", icon: 'pi pi-fw pi-user', to: {name:'profile-supplier'}},
         {label: "Sign out", icon: 'pi pi-fw pi-sign-out', to: "/"},
       ],
